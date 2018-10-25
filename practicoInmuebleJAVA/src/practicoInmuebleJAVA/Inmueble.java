930f3615c087789342518080c4565eacfe3486a0
david <david@sistemas2.panalab.local>
2018-10-24 21:44:42 -0300

    david2

---
+++ practicoInmuebleJAVA/src/practicoInmuebleJAVA/Inmueble.java
@@ -0,0 +1,227 @@
+package practicoInmuebleJAVA;
+
+public class Inmueble {
+	
+	private Propietario propietario;
+	private Agente agente;
+	private String calle;
+	private int nro;
+	private int piso;
+	private String puerta;
+	private String tipoOperacion;
+	private String estado;
+	private String luminicidad;
+	private String distribucion;
+	private double mtotales;
+	private double mcubiertos;
+	private double mdescubiertos;
+	private int nroDormitorios;
+	private int nroBanos;
+	private int nroPlantas;
+	private int nroAscensores;
+	private String orientacion;
+	private String vigilancia;
+	private String estConservacion;
+	private boolean pileta;
+	private String tipoPropiedad;
+	private boolean balcon;
+	private boolean patio;
+	
+	public Inmueble() {
+		super();
+	}
+
+	public Propietario getPropietario() {
+		return propietario;
+	}
+
+	public void setPropietario(Propietario propietario) {
+		this.propietario = propietario;
+	}
+
+	public Agente getAgente() {
+		return agente;
+	}
+
+	public void setAgente(Agente agente) {
+		this.agente = agente;
+	}
+
+	public String getCalle() {
+		return calle;
+	}
+
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+
+	public int getNro() {
+		return nro;
+	}
+
+	public void setNro(int nro) {
+		this.nro = nro;
+	}
+
+	public int getPiso() {
+		return piso;
+	}
+
+	public void setPiso(int piso) {
+		this.piso = piso;
+	}
+
+	public String getPuerta() {
+		return puerta;
+	}
+
+	public void setPuerta(String puerta) {
+		this.puerta = puerta;
+	}
+
+	public String getTipoOperacion() {
+		return tipoOperacion;
+	}
+
+	public void setTipoOperacion(String tipoOperacion) {
+		this.tipoOperacion = tipoOperacion;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getLuminicidad() {
+		return luminicidad;
+	}
+
+	public void setLuminicidad(String luminicidad) {
+		this.luminicidad = luminicidad;
+	}
+
+	public String getDistribucion() {
+		return distribucion;
+	}
+
+	public void setDistribucion(String distribucion) {
+		this.distribucion = distribucion;
+	}
+
+	public double getMtotales() {
+		return mtotales;
+	}
+
+	public void setMtotales(double mtotales) {
+		this.mtotales = mtotales;
+	}
+
+	public double getMcubiertos() {
+		return mcubiertos;
+	}
+
+	public void setMcubiertos(double mcubiertos) {
+		this.mcubiertos = mcubiertos;
+	}
+
+	public double getMdescubiertos() {
+		return mdescubiertos;
+	}
+
+	public void setMdescubiertos(double mdescubiertos) {
+		this.mdescubiertos = mdescubiertos;
+	}
+
+	public int getNroDormitorios() {
+		return nroDormitorios;
+	}
+
+	public void setNroDormitorios(int nroDormitorios) {
+		this.nroDormitorios = nroDormitorios;
+	}
+
+	public int getNroBanos() {
+		return nroBanos;
+	}
+
+	public void setNroBanos(int nroBanos) {
+		this.nroBanos = nroBanos;
+	}
+
+	public int getNroPlantas() {
+		return nroPlantas;
+	}
+
+	public void setNroPlantas(int nroPlantas) {
+		this.nroPlantas = nroPlantas;
+	}
+
+	public int getNroAscensores() {
+		return nroAscensores;
+	}
+
+	public void setNroAscensores(int nroAscensores) {
+		this.nroAscensores = nroAscensores;
+	}
+
+	public String getOrientacion() {
+		return orientacion;
+	}
+
+	public void setOrientacion(String orientacion) {
+		this.orientacion = orientacion;
+	}
+
+	public String getVigilancia() {
+		return vigilancia;
+	}
+
+	public void setVigilancia(String vigilancia) {
+		this.vigilancia = vigilancia;
+	}
+
+	public String getEstConservacion() {
+		return estConservacion;
+	}
+
+	public void setEstConservacion(String estConservacion) {
+		this.estConservacion = estConservacion;
+	}
+
+	public boolean isPileta() {
+		return pileta;
+	}
+
+	public void setPileta(boolean pileta) {
+		this.pileta = pileta;
+	}
+
+	public String getTipoPropiedad() {
+		return tipoPropiedad;
+	}
+
+	public void setTipoPropiedad(String tipoPropiedad) {
+		this.tipoPropiedad = tipoPropiedad;
+	}
+
+	public boolean isBalcon() {
+		return balcon;
+	}
+
+	public void setBalcon(boolean balcon) {
+		this.balcon = balcon;
+	}
+
+	public boolean isPatio() {
+		return patio;
+	}
+
+	public void setPatio(boolean patio) {
+		this.patio = patio;
+	}
+	
+	
+}
--- practicoInmuebleJAVA/src/practicoInmuebleJAVA/Propietario.java
+++ practicoInmuebleJAVA/src/practicoInmuebleJAVA/Propietario.java
@@ -1,5 +1,78 @@
 package practicoInmuebleJAVA;
 
 public class Propietario {
-
+	
+	private String nombre;
+	private String apellido;
+	private String dni;
+	private int telefono;
+	private int telMovil;
+	private String email;
+	private String direccion;
+	private String codpostal;
+	private String localidad;
+	private String comentarios;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public int getTelMovil() {
+		return telMovil;
+	}
+	public void setTelMovil(int telMovil) {
+		this.telMovil = telMovil;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getCodpostal() {
+		return codpostal;
+	}
+	public void setCodpostal(String codpostal) {
+		this.codpostal = codpostal;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public String getComentarios() {
+		return comentarios;
+	}
+	public void setComentarios(String comentarios) {
+		this.comentarios = comentarios;
+	}
+	
+	
 }
