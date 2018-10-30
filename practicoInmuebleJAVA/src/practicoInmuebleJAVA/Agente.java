fd531f785210e4f829c7c4995bae7732ae4486c9
monip37 <monica.piatti@gmail.com>
2018-10-30 20:20:44 -0300

    Agrego clase Persona y modificacion Propietario y Agente

--- practicoInmuebleJAVA/src/practicoInmuebleJAVA/Agente.java
+++ practicoInmuebleJAVA/src/practicoInmuebleJAVA/Agente.java
@@ -1,13 +1,23 @@
 package practicoInmuebleJAVA;
 
-public class Agente extends Propietario {
+public class Agente extends Persona {
 	
 	private int nroagente;
 	private int cuil;
 	private double sueldobasico;
 	
-	public Agente() {
-		super();
+	
+	/**
+	 * @param nroagente
+	 * @param cuil
+	 * @param sueldobasico
+	 */
+	public Agente(String nombre, String apellido, int dni, int telefono, int telMovil, String email, String direccion,
+			String codpostal, String localidad, String comentarios, int nroagente, int cuil, double sueldobasico) {
+		super (nombre, apellido, dni, telefono, telMovil, email, direccion, codpostal, localidad, comentarios);
+		this.nroagente = nroagente;
+		this.cuil = cuil;
+		this.sueldobasico = sueldobasico;
 	}
 
 	public int getNroagente() {
--- practicoInmuebleJAVA/src/practicoInmuebleJAVA/Propietario.java
+++ practicoInmuebleJAVA/src/practicoInmuebleJAVA/Propietario.java
@@ -1,78 +1,25 @@
 package practicoInmuebleJAVA;
 
-public class Propietario {
-	
-	private String nombre;
-	private String apellido;
-	private String dni;
-	private int telefono;
-	private int telMovil;
-	private String email;
-	private String direccion;
-	private String codpostal;
-	private String localidad;
-	private String comentarios;
-	
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getApellido() {
-		return apellido;
-	}
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-	public String getDni() {
-		return dni;
-	}
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-	public int getTelefono() {
-		return telefono;
-	}
-	public void setTelefono(int telefono) {
-		this.telefono = telefono;
-	}
-	public int getTelMovil() {
-		return telMovil;
-	}
-	public void setTelMovil(int telMovil) {
-		this.telMovil = telMovil;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getDireccion() {
-		return direccion;
-	}
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-	public String getCodpostal() {
-		return codpostal;
-	}
-	public void setCodpostal(String codpostal) {
-		this.codpostal = codpostal;
-	}
-	public String getLocalidad() {
-		return localidad;
-	}
-	public void setLocalidad(String localidad) {
-		this.localidad = localidad;
-	}
-	public String getComentarios() {
-		return comentarios;
-	}
-	public void setComentarios(String comentarios) {
-		this.comentarios = comentarios;
+public class Propietario extends Persona{
+
+		
+	/**
+	 * @param nombre
+	 * @param apellido
+	 * @param dni
+	 * @param telefono
+	 * @param telMovil
+	 * @param email
+	 * @param direccion
+	 * @param codpostal
+	 * @param localidad
+	 * @param comentarios
+	 */
+	public Propietario(String nombre, String apellido, int dni, int telefono, int telMovil, String email,
+			String direccion, String codpostal, String localidad, String comentarios) {
+		super(nombre, apellido, dni, telefono, telMovil, email, direccion, codpostal, localidad, comentarios);
+		// TODO Auto-generated constructor stub
 	}
 	
-	
+		
 }
