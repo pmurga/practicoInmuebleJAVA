48434a89eebabb39fb8c2eeb835b51fef18d9ed5
david <david@sistemas2.panalab.local>
2018-11-02 18:41:54 -0300

    UserUI David v2

--- practicoInmuebleJAVA/src/practicoInmuebleJAVA/UI/UserUI.java
+++ practicoInmuebleJAVA/src/practicoInmuebleJAVA/UI/UserUI.java
@@ -32,75 +32,159 @@ public class UserUI {
 	Propietario p4;
 	Propietario p5;
 	
-//	// Creo las variables para ingreso de informacion por pantalla
-//	
-//	Scanner entrada = new Scanner(System.in);
-//	
-//		
-//	// Bienvenida.
-//		System.out.println("Bienvenido a Inmobiliaria CAECE 2018\n");
-//	
-//	// Consulta al usuario por los inmubles que que desea encontrar.
-//	
-//		System.out.println("Que tipo de propiedad esta buscando:\n");
-//		System.out.println("Filtre por estas 5 principales caracteristicas:\n");
-//		System.out.println("Ingresar por favor los nombres exactos que aparecen entre parentesis en cada caracteristica\n");
-//	
-//		System.out.println("Tipo de propiedad: ESTUDIO, LOFT, DEPARTAMENTO, PISO, DUPLEX, TRIPLEX, CHALET, CASA, LOCAL, COCHERA, OFICINA, EDIFICIO");
-//			String tipoProp = entrada.nextLine();
-//		System.out.println("Estado: HABITADO_PROPIETARIO, DESHABITADO, EN_CONSTRUCCION, HABITADO_INQUILINO");
-//			String estado = entrada.nextLine();
-//		System.out.println("Luminocidad: ALTA, MEDIA, BAJA");
-//			String luminocidad = entrada.nextLine();
-//		System.out.println("Vigilancia: VEINTICUATRO_HS, DIURNO, NOCTURNO, NO_TIENE");
-//			String vigilancia = entrada.nextLine();
-//		System.out.println("Estado de conservación: A_ESTRENAR, A_RECICLAR, IMPECABLE, BUENO, MUY_BUENO");
-//			String estConserva = entrada.nextLine();
-//		
-//	//Impresion de los datos ingresados
-//			
-//		System.out.println("Tipo de propiedad:" + tipoProp);
-//		System.out.println("Estado:" + estado);
-//		System.out.println("Luminocidad:" + luminocidad);
-//		System.out.println("Vigilancia:" + vigilancia);
-//		System.out.println("Estado de conservación:" + estConserva);
-//			
-//	//Ejecucion de busqueda de popiedad con los filtros configurados.
-			
-//		//	caece.busqueda(estado, luminocidad, vigilancia, estConserva, tipoProp);
-			
-	
 	// Creo las variables para ingreso de informacion por pantalla
-	
-		String tipoProp;
-		String estado;
-		String luminocidad;
-		String vigilancia;
-		String estConserva;
-			
+		Scanner entrada = new Scanner(System.in);
+		String tipoProp = null;
+		String estado = null;
+		String luminocidad = null;
+		String vigilancia = null;
+		String estConserva = null;
+		
 	// Bienvenida.
-		
-		JOptionPane.showMessageDialog(null,"BIENVENIDOS A LA INMOBILIARIA CAECE 2018\n" + "\n" + "¿Qué tipo de propiedad esta buscando?:\n" + "\n" + "Filtre por estas 5 principales caracteristicas:\n" + "TIPO DE PROPIEDAD, ESTADO, LUMINOCIDAD, VIGILANCIA, ESTADO DE CONSERVACION\n" + "(Ingresar por favor los nombres exactos que aparecen entre parentesis en cada caracteristica:)");
-		
+	
 	// Consulta al usuario por los inmubles que que desea encontrar.
 		
-		tipoProp = JOptionPane.showInputDialog(null, "Iipo de propiedad:\n" + "\n" + "ESTUDIO, LOFT, DEPARTAMENTO, PISO, DUPLEX, TRIPLEX, CHALET, CASA, LOCAL, COCHERA, OFICINA, EDIFICIO");
-		estado = JOptionPane.showInputDialog(null, "Estado:\n" + "\n" + "HABITADO_PROPIETARIO, DESHABITADO, EN_CONSTRUCCION, HABITADO_INQUILINO");
-		luminocidad = JOptionPane.showInputDialog(null, "Luminocidad:\n" + "\n" + "ALTA, MEDIA, BAJA");
-		vigilancia = JOptionPane.showInputDialog(null, "Vigilancia: \n" + "\n" + "VEINTICUATRO_HS, DIURNO, NOCTURNO, NO_TIENE");
-		estConserva = JOptionPane.showInputDialog(null, "Estado de conservación:\n" + "\n" + "A_ESTRENAR, A_RECICLAR, IMPECABLE, BUENO, MUY_BUENO");
-
-		
-	// Impresion de los datos ingresados				
+		System.out.println("BIENVENIDOS A INMOVILIARIA CAECE 2018\n");
+		System.out.println("Que tipo de propiedad esta buscando?:\n");
+		System.out.println("Filtre por estas 5 principales caracteristicas:\n");
+		System.out.println("Las opcioens estan numeradas, por favor elIja el numero de la opcion que desee\n");
+	
+		System.out.println("A - Tipo de propiedad:\n");   
+		System.out.println ( "1 - ESTUDIO");
+		System.out.println ( "2 - LOFT");
+		System.out.println ( "3 - DEPARTAMENTO");
+		System.out.println ( "4 - PISO");
+		System.out.println ( "5 - DUPLEX");
+		System.out.println ( "6 - TRIPLEX");
+		System.out.println ( "7 - CHALET");
+		System.out.println ( "8 - CASA");
+		System.out.println ( "9 - LOCAL");
+		System.out.println ( "10 - COCHERA");
+		System.out.println ( "11 - OFICINA");
+		System.out.println ( "12 - EDIFICIO");
+			String valor = entrada.nextLine();
+			
+			switch (valor) {
+			case "1":
+				tipoProp = "ESTUDIO"; break;
+			case "2":
+				tipoProp = "LOFT"; break;
+			case "3":
+				tipoProp = "DEPARTAMENTO"; break;
+			case "4":
+				tipoProp = "PISO"; break;
+			case "5":
+				tipoProp = "DUPLEX"; break;
+			case "6":
+				tipoProp = "TRIPLEX"; break;
+			case "7":
+				tipoProp = "CHALET"; break;
+			case "8":
+				tipoProp = "CASA"; break;
+			case "9":
+				tipoProp = "LOCAL"; break;
+			case "10":
+				tipoProp = "COCHERA"; break;
+			case "11":
+				tipoProp = "OFICINA"; break;
+			case "12":
+				tipoProp = "EDIFICIO"; break;
+			}
+				
+		System.out.println("B - Estado:\n");   
+		System.out.println ( "1 - HABITAOD POR PROPIETARIO");
+		System.out.println ( "2 - HABITADO POR INQUILINO");
+		System.out.println ( "3 - DESHABITADO");
+		System.out.println ( "4 - EN CONSTRUCCION");
+			 valor = entrada.nextLine();
+					
+			switch (valor) {
+			case "1":
+				estado = "HABITADO_PROPIETARIO"; break;
+			case "2":
+				estado = "HABITADO_INQUILINO"; break;
+			case "3":
+				estado = "DESHABITADO"; break;
+			case "4":
+				estado = "EN_CONTRUCCION"; break;
+			}
+					
+		System.out.println("C - Luminocidad:\n");   
+		System.out.println ( "1 - ALTA");
+		System.out.println ( "2 - MEDIA");
+		System.out.println ( "3 - BAJA");
+			valor = entrada.nextLine();
+							
+			switch (valor) {
+			case "1":
+				luminocidad = "ALTA"; break;
+			case "2":
+				luminocidad = "MEDIA"; break;
+			case "3":
+				luminocidad = "BAJA"; break;
+			}
+			
+				
+		System.out.println("D - Vigilancia:\n");   
+		System.out.println ( "1 - 24 HS");
+		System.out.println ( "2 - DIURNO");
+		System.out.println ( "3 - NOCTURNO");
+		System.out.println ( "4 - SIN VIGILANCIA");
+			valor = entrada.nextLine();
+									
+			switch (valor) {
+			case "1":
+				vigilancia = "VEINTICUATRO_HS"; break;
+			case "2":
+				vigilancia = "DIURNO"; break;
+			case "3":
+				vigilancia = "NOCTURNO"; break;				
+			case "4":
+				vigilancia = "NO_TIENE"; break;			
+			}	
+				
+		System.out.println("D - Estdo de conservacion:\n");   
+		System.out.println ( "1 - A ESTRENAR");
+		System.out.println ( "2 - A RECICLAR");
+		System.out.println ( "3 - IMPECABLE");
+		System.out.println ( "4 - MUY BUENO");
+		System.out.println ( "5 - BUENO");
+			valor = entrada.nextLine();
+										
+			switch (valor) {
+			case "1":
+				estConserva = "A_ESTRENAR"; break;
+			case "2":
+				estConserva = "A_ESTRENAR"; break;
+			case "3":
+				estConserva = "IMPECABLE"; break;				
+			case "4":
+				estConserva = "BUENO"; break;				
+			case "5":
+				estConserva = "MUY_BUENO"; break;
+			}
+				
+		//Impresion de los datos ingresados
+			
 		System.out.println("Tipo de propiedad:" + tipoProp);
 		System.out.println("Estado:" + estado);
 		System.out.println("Luminocidad:" + luminocidad);
 		System.out.println("Vigilancia:" + vigilancia);
 		System.out.println("Estado de conservación:" + estConserva);
-		
-	// Ejecucion de busqueda de popiedad con los filtros configurados.
 			
-	//	caece.busqueda(estado, luminocidad, vigilancia, estConserva, tipoProp);	
+	//Ejecucion de busqueda de popiedad con los filtros configurados.
+		
+		//	caece.busqueda(estado, luminocidad, vigilancia, estConserva, tipoProp);
+			
+	//Procedimiento para mostrar todas las popiedades encontradas numeras con todas sus propiedades para que el usuario elija la quedese
+		
+	//Eleccion de tipo de operacion
+		
+	// Operacion
+		
+	//Operaciones postOperacion solicitadas
+		
+		
 			
 	}
 	
